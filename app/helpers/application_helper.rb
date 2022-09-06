a6edd3c647ebe4b3ba31af36195b5df2875a10b5
EC2 Default User <ec2-user@ip-172-31-18-61.ap-northeast-1.compute.internal>
2022-09-06 04:01:43 +0000

    viewの編集2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,24 @@ module ApplicationHelper
   
   
   
+  def tax_price(price)
+    (price * 1.1).floor
+  end
+
+  def sub_price(sub)
+    (tax_price(sub.item.price) * sub.amount)
+  end
+
+  def total_price(totals)
+    price = 0
+    totals.each do |total|
+      price += sub_price(total)
+    end
+    return price
+  end
+  
+  def current_cart
+    @cart_items = current_customer.cart_items
+  end
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -11,4 +11,12 @@ class Item < ApplicationRecord
   validates :introduction, length: {maximum: 200}
   validates :price, numericality: { only_integer: true }
   
+  def get_image
+    unless image.attached?
+       file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+     image
+  end
+  
 end
